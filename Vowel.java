f786852754e7fbc917eb27fcfa0edc6c65dcd986
vigneshraj <vignesh22raj@gmail.com>
2017-07-11 11:18:23 +0530

    Create Vowel.java

---
+++ Vowel.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+class Vowel {
+  public static void main(String args[]) throws Exception
+  {
+  String b;
+  char a;
+ 
+  BufferedReader vc=new BufferedReader(new InputStreamReader(System.in));
+  b=vc.readLine();
+  a=b.charAt(0);
+  if(a=='A' || a=='E'|| a=='I' || a=='O' || a=='U' ||a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+  {
+  System.out.println(a+" is a Vowel");
+  }
+  else
+  {
+  System.out.println(a+" is not a Vowel");
+  }
+  }
+}

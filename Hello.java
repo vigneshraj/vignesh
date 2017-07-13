011021b4c0637f046ff0e51cf6a19da054902ce1
vigneshraj <vignesh22raj@gmail.com>
2017-07-13 17:39:01 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+class Hello {
+  public staic void main(String args[]) 
+  {
+    String a="Hello";
+    for(int i=0;i<5;i++)
+    {
+    System.out.println(a);
+    }
+  }
+}

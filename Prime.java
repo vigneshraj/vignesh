5207208b433bda154ef21f293fb0505817e3ab69
vigneshraj <vignesh22raj@gmail.com>
2017-07-14 15:04:47 +0530

    Update Prime.java

--- Prime.java
+++ Prime.java
@@ -3,28 +3,28 @@ public class Prime
 {
     public static void main(String args[])
     {
-         int s1, s2, s3, flag = 0, i, j;
-         Scanner s = new Scanner(System.in);
-         System.out.println ("Enter the lower limit :"); 
-         s1 = s.nextInt();
-         System.out.println ("Enter the upper limit :"); 
-         s2 = s.nextInt();
-         System.out.println ("The prime numbers in between the entered limits are :");
-         for(i = s1; i <= s2; i++)
+         int a1, a2, a3, fl = 0, i, j;
+         Scanner vc = new Scanner(System.in);
+         System.out.println ("Enter the Starting limit :"); 
+         a1 = vc.nextInt();
+         System.out.println ("Enter the Ending limit :"); 
+         a2 = vc.nextInt();
+         System.out.println ("The prime numbers in between"+a1+" and"+a2+" is :");
+         for(i = a1; i <= a2; i++)
          {
              for( j = 2; j < i; j++)
              {
                  if(i % j == 0)
                  {
-                     flag = 0;
+                     fl = 0;
                      break;
                  }
                  else
                  {
-                     flag = 1;
+                     fl = 1;
                  }
              }
-             if(flag == 1)
+             if(fl == 1)
              {
                  System.out.println(i);
              }

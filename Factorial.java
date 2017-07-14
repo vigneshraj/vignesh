5c2931f0a765c9f3f3acf4e4263da997372908c9
vigneshraj <vignesh22raj@gmail.com>
2017-07-14 12:30:49 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+class Factorial {
+  public static void main(String args[]) {
+    int i,sum=1;
+    int n;
+    Scanner vc=new Scanner(System.in);
+    System.out.println("Enter the range of factorial:");
+    n=vc.nextInt();
+    for(i=1;i<=n;i++)
+    {
+      sum=sum*i;
+    }
+    System.out.println("Enter the range of factorial is:"+sum);
+  }
+}
